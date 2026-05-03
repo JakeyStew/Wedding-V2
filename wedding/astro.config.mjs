f3398bc7e6c32db5c4838df6ae5cffe72c10eece
Jake Stewart <42218259+JakeyStew@users.noreply.github.com>
2026-05-03 22:26:08 +0100

    Updatee: Typo in config

--- wedding/astro.config.mjs
+++ wedding/astro.config.mjs
@@ -6,6 +6,6 @@ import tunnel from 'astro-tunnel';
 // https://astro.build/config
 export default defineConfig({
   site: "https://jakeystew.github.io",
-  base: "/Wedding-V2/",
+  base: "/Wedding-V2",
   integrations: [tunnel()]
 });
